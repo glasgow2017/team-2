6c54fad256730fd591f96f3f04c37c5ac035aec3
benjylxwang <benjylxwang@hotmail.com>
2017-11-04 15:31:47 +0000

    Created manifest.json for extension. Added an icon.

--- js/navigation/navigation.js
+++ js/navigation/navigation.js
@@ -1,3 +1,19 @@
 function Navigator() {
-    
+    var displayableDivs = [];
+
+    var parsePage = function() {
+        var pageDivs = $("div").get();
+
+        for (var div in pageDivs) {
+            if (pageDivs.hasOwnProperty(div) && div.hasAttr("role")) {
+                // Add div to list of displayable divs
+                displayableDivs.push(div);
+                console.log("Displayable Div: " + div);
+            }
+        }
+    }
 }
+
+$.fn.hasAttr = function(name) {
+    return this.attr(name) !== undefined || this.attr(name) !== false;
+};
