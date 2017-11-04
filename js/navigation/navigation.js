a83ae750576414a489c0f878693cb6a640fa9ac9
benjylxwang <benjylxwang@hotmail.com>
2017-11-04 17:10:39 +0000

    Completed processing page function.

--- js/navigation/navigation.js
+++ js/navigation/navigation.js
@@ -1,18 +1,60 @@
+var MAIN = "main";
+var NAVIGATION = "navigation";
+var SEARCH = "search";
+var FORM = "form";
+var CONTENTINFO = "contentinfo";
+var COMPLEMENTARY = "complementary";
+var BANNER = "banner";
 
 var displayableDivs = [];
+var mainDiv;
+var navDiv;
+var landmarkDivs = [];
+var currentView = $(document);
 
-function parsePage() {
+function processDisplayableDivs() {
     var pageDivs = $("div").get();
+    var foundMain = false;
+    var foundNav = false;
 
     for (var div in pageDivs) {
         if (pageDivs.hasOwnProperty(div) && $(div).is("[role]")) {
             // Add div to list of displayable divs
             displayableDivs.push(div);
             console.log("Displayable Div: " + div);
+
+            var divRole = div.attr("role");
+            if (divRole === MAIN) {
+                mainDiv = div;
+                foundMain = true;
+            } else if (divRole === NAVIGATION) {
+                navDiv = div;
+                foundNav = true;
+            } else if (divRole === SEARCH || divRole === FORM || divRole === CONTENTINFO || divRole === COMPLEMENTARY ||
+                divRole === BANNER) {
+                landmarkDivs.push(div);
+            }
+        }
+
+        // If found two most important elements, start speaking
+        if (foundMain && foundNav) {
+            speakToUser();
         }
     }
-
-    return null;
 }
 
-parsePage();
+function speakToUser() {
+    // Overall page description (e.g. page title, company name)
+    var overallDescription = "This page is mainly about ___.";
+
+    // Nav
+    var navigationSpeak = "To navigate to another page on this website, press 1";
+
+    // Main
+
+    for (var block in landmarkDivs) {
+
+    }
+}
+
+
