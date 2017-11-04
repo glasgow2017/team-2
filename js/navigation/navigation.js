ea55cc70e8ffc389b151e536e4d536bd03c1f3b3
benjylxwang <benjylxwang@hotmail.com>
2017-11-04 16:41:54 +0000

    Added content_script to manifest file.

--- js/navigation/navigation.js
+++ js/navigation/navigation.js
@@ -1,19 +1,18 @@
-function Navigator() {
-    var displayableDivs = [];
 
-    var parsePage = function() {
-        var pageDivs = $("div").get();
+var displayableDivs = [];
 
-        for (var div in pageDivs) {
-            if (pageDivs.hasOwnProperty(div) && div.hasAttr("role")) {
-                // Add div to list of displayable divs
-                displayableDivs.push(div);
-                console.log("Displayable Div: " + div);
-            }
+function parsePage() {
+    var pageDivs = $("div").get();
+
+    for (var div in pageDivs) {
+        if (pageDivs.hasOwnProperty(div) && $(div).is("[role]")) {
+            // Add div to list of displayable divs
+            displayableDivs.push(div);
+            console.log("Displayable Div: " + div);
         }
     }
+
+    return null;
 }
 
-$.fn.hasAttr = function(name) {
-    return this.attr(name) !== undefined || this.attr(name) !== false;
-};
+parsePage();
