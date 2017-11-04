e21ebcd00d7958249ad9428b670a15d484cba3db
benjylxwang <benjylxwang@hotmail.com>
2017-11-04 17:35:48 +0000

    Got text-to-speech responsive voice API

--- js/navigation/navigation.js
+++ js/navigation/navigation.js
@@ -6,55 +6,39 @@ var CONTENTINFO = "contentinfo";
 var COMPLEMENTARY = "complementary";
 var BANNER = "banner";
 
-var displayableDivs = [];
-var mainDiv;
-var navDiv;
-var landmarkDivs = [];
+var currentDisplayElements = [];
 var currentView = $(document);
 
-function processDisplayableDivs() {
-    var pageDivs = $("div").get();
+function processTopLevel() {
+    var pageDivs = $("body").children();
     var foundMain = false;
     var foundNav = false;
 
     for (var div in pageDivs) {
         if (pageDivs.hasOwnProperty(div) && $(div).is("[role]")) {
-            // Add div to list of displayable divs
-            displayableDivs.push(div);
-            console.log("Displayable Div: " + div);
-
             var divRole = div.attr("role");
-            if (divRole === MAIN) {
-                mainDiv = div;
-                foundMain = true;
-            } else if (divRole === NAVIGATION) {
-                navDiv = div;
-                foundNav = true;
-            } else if (divRole === SEARCH || divRole === FORM || divRole === CONTENTINFO || divRole === COMPLEMENTARY ||
-                divRole === BANNER) {
-                landmarkDivs.push(div);
+            if (divRole === SEARCH || divRole === FORM || divRole === CONTENTINFO || divRole === COMPLEMENTARY ||
+            divRole === BANNER || divRole === MAIN || divRole === NAVIGATION) {
+                // Add div to list of displayable divs
+                currentDisplayElements.push(div);
+                console.log("Displayable Div: " + div);
             }
-        }
-
-        // If found two most important elements, start speaking
-        if (foundMain && foundNav) {
-            speakToUser();
         }
     }
 }
 
-function speakToUser() {
-    // Overall page description (e.g. page title, company name)
-    var overallDescription = "This page is mainly about ___.";
+function getElements(selectedElement) {
+    return $(selectedElement).children();
+}
 
-    // Nav
-    var navigationSpeak = "To navigate to another page on this website, press 1";
-
-    // Main
-
-    for (var block in landmarkDivs) {
-
+function readOutElementList(list) {
+    for (var element in list) {
+        readElementInfo(element);
     }
+}
+
+function readElementInfo(element) {
+    // Read info
 }
 
 
