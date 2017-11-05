8a2b43ee91ad9acf90ff8d99cf220e4f98a8c669
Oscar <oplde@st-andrews.ac.uk>
2017-11-05 05:58:37 +0000

    #ThisAgain server degub

--- Node/Server.js
+++ Node/Server.js
@@ -14,7 +14,7 @@ fs.readFile("MeaningCloudAPIKey",function(err,data){
 });
 
 TopicExtraction = function(req,res){
-  console.log("\n\n\n\n"+req.body.get("text")+"\n\n\n\n\n");
+  console.log("\n\n\n\n"+req.body.["text"]+"\n\n\n\n\n");
 
   var data  = ApiQUery(req.body.text);
   var i= 0;
